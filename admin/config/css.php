80068f0af9d10fb95e07d4c905215732c1631972
creptor-git <jitellezc@gmail.com>
2015-03-25 23:39:06 -0300

    The files
    These're the files for this repository

---
+++ admin/config/css.php
@@ -0,0 +1,87 @@
+<?php
+// CSS:
+?>
+
+<!-- Latest compiled and minified CSS -->
+<link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.0.3/css/bootstrap.min.css">
+
+<!-- Optional theme -->
+<link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.0.3/css/bootstrap-theme.min.css">
+
+<!-- jQuery CSS -->
+<link rel="stylesheet" href="//code.jquery.com/ui/1.10.4/themes/smoothness/jquery-ui-custom.css">
+
+<!-- Dropzone CSS -->
+<link rel="stylesheet" href="css/dropzone.css">
+
+<!-- Atom.SaveOnBlur CSS -->
+<link rel="stylesheet" href="css/jquery.atom.SaveOnBlur.css">
+
+<!-- FontAwesome -->
+<link href="//netdna.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.css" rel="stylesheet">
+
+<!-- Navigation Css -->
+<link rel="stylesheet" href="css/fontawesome-iconpicker.min.css"/>
+    <link rel="stylesheet" href="css/default.css" />
+
+<style>
+	
+	html,
+	body {
+	  height: 100%;
+	  /* The html and body elements cannot have any padding or margin. */
+	}
+	
+	/* Wrapper for page content to push down footer */
+	#wrap {
+	  min-height: 100%;
+	  height: auto;
+	  /* Negative indent footer by its height */
+	  margin: 0 auto -60px;
+	  /* Pad bottom by footer height */
+	  padding: 0 0 60px;
+	}
+	
+	/* Set the fixed height of the footer here */
+	#footer {
+	  height: 60px;
+	  background-color: #f5f5f5;
+	}
+	
+	#btn-debug {
+		/*
+		position: absolute;
+		right: 5px;
+		*/
+	}	
+	
+	#console-debug {
+		position: absolute;
+		top: 50px;
+		left:0px;
+		width:30%;
+		height:700px;
+		overflow-y:scroll;
+		background-color: #FFFFFF;
+		box-shadow: 2px 2px 5px #CCCCCC;
+	}
+	#console-debug pre {
+		
+		
+	}
+	
+	.avatar-container {
+		
+		width: 100px;
+		height: 100px;
+		
+		border-radius:3px;
+		
+		background-size: cover;
+		background-position: center center;
+		
+		
+	}
+	
+	
+</style>
